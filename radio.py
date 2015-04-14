896ec5116ec50484d5aabf705e921d3ff614795b
reiser4 <reiser4@gmail.com>
2015-04-14 17:09:14 +0200

    init and read radio ptt

--- radio.py
+++ radio.py
@@ -4,6 +4,22 @@
 # gestisce l'interfaccia con i due transceiver a livello di PTT e interlock.
 
 class Radio:
+	def __init__ (self, inhibitInPin, inhibitOutPin, pttIn, pttOut, pttOutD1, pttOutD2)
+		print "Inizializzata Radio con pin: ", inhibitInPin, inhibitOutPin, pttIn,
+			pttOut, pttOutD1, pttOutD2
+
+		self.inhibitInPin = inhibitInPin
+		self.inhibitOutPin = inhibitOutPin
+		self.pttIn = pttIn
+		self.pttOut = pttOut
+		self.pttOutD1 = pttOutD1
+		self.pttOutD2 = pttOutD2
+
+		GPIO.setup(pttIn, GPIO.IN, PUD_UP)
+
 	def readPTT(self):
-		print "Lettura PTT non implementata"
-		return False
+		val = GPIO.input(self.pttIn)
+		print "Valore PTT: ", val
+		return val
+		#print "Lettura PTT non implementata"
+		#return False
