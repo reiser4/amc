6a62e98c12e80f0dcee0b61f426d450a9c9463e0
giulioribe <giulio.ribe@gmail.com>
2015-06-20 15:08:21 +0200

    now configuration-panel.py can save configuration

--- configuration-panel.py
+++ configuration-panel.py
@@ -1,9 +1,15 @@
-import sys
+import sys, os
 from PyQt5.QtWidgets import (QWidget,QApplication, QDesktopWidget, QPushButton,
     QMessageBox, QMainWindow, QAction, qApp, QTextEdit, QLCDNumber, QSlider, QVBoxLayout,
-    QGridLayout, QTabWidget, QFileDialog, QCheckBox, QLabel, QLineEdit)
+    QGridLayout, QTabWidget, QFileDialog, QCheckBox, QLabel, QLineEdit, QAbstractButton)
 from PyQt5.QtGui import QIcon, QKeySequence
 from PyQt5.QtCore import QCoreApplication, Qt
+from atomicwrite import AtomicWrite
+
+'''
+Riga 0:     Active(0,0)  Label(0,1)   Radio1(0,2+25)  Radio2(0,27+25)
+Riga 1-16:
+'''
 
 # grid = [[" " for x in range(5)] for y in range(5)]
 class ConfigurationPanel(QMainWindow):
@@ -14,6 +20,8 @@ class ConfigurationPanel(QMainWindow):
 
     def initUI(self):
         self.bands = ["6m", "10m", "12m", "15m", "17m", "20m", "30m", "40m", "60m", "80m", "160m"]
+        self.nrow = 16
+        self.nrele = 24
         # imposto le dimensioni della finestra
         self.setGeometry(0, 0, 1500, 600)
         # imposto il titolo della finestra
@@ -42,12 +50,20 @@ class ConfigurationPanel(QMainWindow):
         self.exitAction.setStatusTip("Exit application")
         # collego l'azione exitAction all'evento self.close (gia' presente in PyQT)
         self.exitAction.triggered.connect(self.close)
-        # azione per caricare il file
+
+        # azione per caricare la configurazione da file
         self.openAction = QAction("&Load to file", self)
         self.openAction.setShortcut("Ctrl+L")
         self.openAction.setStatusTip("Load to file")
         # collego l'azione openAction al mio evento che ho creato showDialog
         self.openAction.triggered.connect(self.showDialog)
+
+        # azione per salvare la configurazione su file
+        self.saveAction = QAction("&Save on file", self)
+        self.saveAction.setShortcut("Ctrl+S")
+        self.saveAction.setStatusTip("Save to file")
+        self.saveAction.triggered.connect(self.saveConfiguration)
+
         """
         serialAction = QAction("&Serial", self)
         serialAction.setShortcut("")
@@ -63,11 +79,6 @@ class ConfigurationPanel(QMainWindow):
         UploadAction.setShortcut("")
         UploadAction.setStatusTip("")
         UploadAction.triggered.connect()
-
-        saveAction = QAction("&Save on file", self)
-        saveAction.setShortcut("")
-        saveAction.setStatusTip("")
-        saveAction.triggered.connect()
         """
 
 
@@ -80,8 +91,9 @@ class ConfigurationPanel(QMainWindow):
         filemenu.addAction(self.exitAction)
 
         configuration_menu = menubar.addMenu("&Configuration")
+        configuration_menu.addAction(self.saveAction)
         configuration_menu.addAction(self.openAction)
-        connection_menu = menubar.addMenu("&Connection")
+        #connection_menu = menubar.addMenu("&Connection")
 
     def initTab(self):
         # creo un TabWidget che sara' il widget padre
@@ -89,10 +101,10 @@ class ConfigurationPanel(QMainWindow):
         labels_name = ["Active", "Label", "Bands"]
         tab_list = list()
         self.tablayout_list = list()
-        checkbox_matrix = list()
-        labels_matrix = list()
-        radio1cb_matrix = list()
-        radio2cb_matrix = list()
+        self.checkbox_matrix = list()
+        self.labels_matrix = list()
+        self.radio1cb_matrix = list()
+        self.radio2cb_matrix = list()
         for i in range(len(self.bands)):
             # aggiungo alla lista un oggetto QWidget discendente dal padre
             tab_list.append(QWidget(self.tab_widget))
@@ -105,29 +117,37 @@ class ConfigurationPanel(QMainWindow):
             # nella i-esima tab creo un layout a griglia e posiziono i vari elementi
             self.tablayout_list[i].addWidget(QLabel("Active", tab_list[i]), 0, 0)
             self.tablayout_list[i].addWidget(QLabel("Label", tab_list[i]), 0, 1)
-            self.tablayout_list[i].addWidget(QLabel("Radio 1", tab_list[i]), 0, 2, 1, 25)
-            self.tablayout_list[i].addWidget(QLabel("Radio 2", tab_list[i]), 0, 27, 1, 25)
-            checkbox_matrix.append(list())
-            labels_matrix.append(list())
-            radio1cb_matrix.append(list())
-            radio2cb_matrix.append(list())
+            self.tablayout_list[i].addWidget(QLabel("Radio 1", tab_list[i]), 0, 2, 1, self.nrele)
+            self.tablayout_list[i].addWidget(QLabel("Radio 2", tab_list[i]), 0, 26, 1, self.nrele)
+            self.checkbox_matrix.append(list())
+            self.labels_matrix.append(list())
+            self.radio1cb_matrix.append(list())
+            self.radio2cb_matrix.append(list())
             # per ogni tab devo creare tutti i suoi oggetti al suo interno
-            for y in range(16):
-                checkbox_matrix[i].append(QCheckBox(tab_list[i]))
-                checkbox_matrix[i][y].stateChanged.connect(self.changeCheckBoxState)
-                self.tablayout_list[i].addWidget(checkbox_matrix[i][y], y+1, 0)
-                labels_matrix[i].append(QLineEdit(tab_list[i]))
-                self.tablayout_list[i].addWidget(labels_matrix[i][y], y+1, 1)
-                radio1cb_matrix[i].append(list())
-                radio2cb_matrix[i].append(list())
-                for z in range(24):
-                    radio1cb_matrix[i][y].append(QCheckBox(tab_list[i]))
-                    radio1cb_matrix[i][y][z].stateChanged.connect(self.changeCheckBoxState)
-                    self.tablayout_list[i].addWidget(radio1cb_matrix[i][y][z], y+1, z+2)
+            for y in range(self.nrow):
+                self.checkbox_matrix[i].append(QCheckBox(tab_list[i]))
+                self.checkbox_matrix[i][y].setCheckState(Qt.Unchecked)
+                self.checkbox_matrix[i][y].stateChanged.connect(self.changeCheckBoxState)
+                self.tablayout_list[i].addWidget(self.checkbox_matrix[i][y], y+1, 0)
+                self.labels_matrix[i].append(QLineEdit(tab_list[i]))
+                self.labels_matrix[i][y].setEnabled(False)
+                self.tablayout_list[i].addWidget(self.labels_matrix[i][y], y+1, 1)
+                self.radio1cb_matrix[i].append(list())
+                self.radio2cb_matrix[i].append(list())
+                for z in range(self.nrele):
+                    self.radio1cb_matrix[i][y].append(QCheckBox(tab_list[i]))
+                    self.radio1cb_matrix[i][y][z].stateChanged.connect(self.changeCheckBoxState)
+                    self.radio1cb_matrix[i][y][z].setEnabled(False)
+                    # aggiungo 1 a y perche' devo contare le etichette (Active, Label, Radio1, Radio2)
+                    # aggiungo 2 a z perche' devo tenere conto dei widget di sinistra (checkbox e label)
+                    self.tablayout_list[i].addWidget(self.radio1cb_matrix[i][y][z], y+1, z+2)
 
-                    radio2cb_matrix[i][y].append(QCheckBox(tab_list[i]))
-                    radio2cb_matrix[i][y][z].stateChanged.connect(self.changeCheckBoxState)
-                    self.tablayout_list[i].addWidget(radio2cb_matrix[i][y][z], y+1, z+27)
+                    self.radio2cb_matrix[i][y].append(QCheckBox(tab_list[i]))
+                    self.radio2cb_matrix[i][y][z].stateChanged.connect(self.changeCheckBoxState)
+                    self.radio2cb_matrix[i][y][z].setEnabled(False)
+                    # aggiungo 1 a y perche' devo contare le etichette (Active, Label, Radio1, Radio2)
+                    # aggiungo 2 a z perche' devo tenere conto dei widget di sinistra (checkbox, label e 24 checkbox)
+                    self.tablayout_list[i].addWidget(self.radio2cb_matrix[i][y][z], y+1, z+26)
 
 
     def centerOnScreen(self):
@@ -160,7 +180,7 @@ class ConfigurationPanel(QMainWindow):
 
     def showDialog(self):
         # apre una finestra di selezione in /home dove e' possibile selezionare solo file py
-        fname = QFileDialog.getOpenFileName(self, 'Load file', '/home', "Python Files (*.py)")
+        fname = QFileDialog.getOpenFileName(self, 'Load file', os.getcwd(), "Python Files (*.py)")
         print(fname[0])
         f = open(fname[0], 'r')
         with f:
@@ -173,36 +193,88 @@ class ConfigurationPanel(QMainWindow):
         Funzione che stampa a terminale tutte le volte che viene cliccata
         una checkbox
         '''
-        #if state == Qt.Checked:
-        if True:
+        if state == Qt.Checked or state == Qt.Unchecked:
+        #if True:
             sender = self.sender()
-            self.statusBar().showMessage(sender.text())
-            currentIndex = self.tab_widget.currentIndex()
+            #self.statusBar().showMessage(sender.text())
+            indexTab = self.tab_widget.currentIndex()
             """
             button = self.sender()
             idx = self.layout.indexOf(button)
             location = self.layout.getItemPosition(idx)
             print "Button", button, "at row/col", location[:2]
             """
-            idx = self.tablayout_list[currentIndex].indexOf(sender)
-            location = self.tablayout_list[currentIndex].getItemPosition(idx)
-            print("Tab:", currentIndex, "Row", location[0], "Column", location[1], "Sender:", sender.text(), "State:",state)
+            idx = self.tablayout_list[indexTab].indexOf(sender)
+            location = self.tablayout_list[indexTab].getItemPosition(idx)
+            indexRow = location[0]
+            indexColumn = location[1]
+            print "indexTab: ", indexTab
+            print "idx:", idx
+            text = "Tab: " + str(indexTab) + ", Row: " + str(indexColumn) + ", Column: " + str(indexRow) + ", State: " + str(state)
+            print text
+            self.statusBar().showMessage(text)
 
             #trovo la banda
-            band = self.bands[currentIndex]
-            presetnumber = location[0]
-            if location[1] == 0:
+            band = self.bands[indexTab]
+            presetnumber = indexRow
+            # se sono nella prima colonna entro nel ramo True
+            if indexColumn == 0:
                 ptype = "active"
-            else:
-                if location[1] > 1 and location[1] < 26:
-                    ptype = "radioA, relay" + str(location[1]-1)
+                if state == Qt.Checked:
+                    self.labels_matrix[indexTab][indexRow-1].setEnabled(True)
+                    for i in range(self.nrele):
+                        # -1 perche' e' compresa la riga delle etichette
+                        self.radio1cb_matrix[indexTab][indexRow-1][i].setEnabled(True)
+                        self.radio2cb_matrix[indexTab][indexRow-1][i].setEnabled(True)
                 else:
-                    ptype = "radioB, relay" + str(location[1]-26)#attenzione alla "label"
+                    self.labels_matrix[indexTab][indexRow-1].setEnabled(False)
+                    for i in range(self.nrele):
+                        self.radio1cb_matrix[indexTab][indexRow-1][i].setEnabled(False)
+                        self.radio2cb_matrix[indexTab][indexRow-1][i].setEnabled(False)
+            else:
+                if indexColumn > 1 and indexColumn < 26:
+                    ptype = "radioA, relay " + str(location[1]-1)
+                else:
+                    # -25 perche' sarebbe -26 (numero oggetti) + 1 (perche' non voglio partire da 0 ma da 1)
+                    ptype = "radioB, relay " + str(location[1]-25) # attenzione alla "label"
 
-            print ("Posizione decodificata: banda",band,", preset numero",presetnumber,", tipo",ptype)
- 
+            print ("Posizione decodificata: banda ",band,", preset numero ",presetnumber,", tipo ",ptype)
+
+    def saveConfiguration(self):
+
+        tmpwrite = '{"Configuration":{"Relay":'
+        for indexTab in range(len(self.bands)):
+            firstWrite = True
+            for indexRow in range(self.nrow):
+                if self.checkbox_matrix[indexTab][indexRow].isChecked() == True:
+                    isChecked = True
+                    if firstWrite:
+                        tmpwrite += '{"' + self.bands[indexTab] + '":'
+                        firstWrite = False
+                    tmpwrite += '{"' + str(indexRow) + '":{"Label":' + '"' + self.labels_matrix[indexTab][indexRow].text() + '",'
+                    relayA = ""
+                    relayB = ""
+                    for z in range(self.nrele):
+                        if self.radio1cb_matrix[indexTab][indexRow][z].isChecked() == True:
+                            relayA += "1"
+                        else:
+                            relayA += "0"
+                        if self.radio2cb_matrix[indexTab][indexRow][z].isChecked() == True:
+                            relayB += "1"
+                        else:
+                            relayB += "0"
+                    tmpwrite += '"RelayA":' + '{"' + relayA + '"},"RelayB":' + '{"' + relayB + '"}}}'
+            if isChecked:
+                tmpwrite += '}'
+                isChecked = False
+            firstWrite =True
+        tmpwrite += '}}'
+        AtomicWrite.writeFile("test-config.json", tmpwrite)
+        print "Scrittura eseguita correttamente su file"
+
+
+
 if __name__ == '__main__':
-
     # Every PyQt5 application must create an application object.
     app = QApplication(sys.argv)
     configuration_panel = ConfigurationPanel()
